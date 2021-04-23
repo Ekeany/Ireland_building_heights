d73b032768cd3b2a95830991769f0577835a236b
Eoghan <egnkeany@gmail.com>
2021-04-24 00:05:26 +0100

    added the same test sampling as found in Unet

--- Modelling/DataSetCreation/take_sample.py
+++ Modelling/DataSetCreation/take_sample.py
@@ -172,6 +172,250 @@ def take_samples(directory, stratified_sample, add_to_name=''):
     return stratified_sample
 
 
+def create_directory_to_save_to(output_dir):
+    '''
+    checks to see if directory exists if it doesn't it
+    will create it.
+    '''
+    if output_dir is not None and not os.path.exists(output_dir):
+        print(f'creating the directory {output_dir} as it does not exist.')
+        os.makedirs(output_dir)
+
+
+
+def start_points(size, chunk_size, overlap=0):
+    '''
+    split an array into equal parts including overlap. used
+    as the starting points then we add the size to each point to get
+    the desired result.
+    
+    chunk_size: is the size of the stride length to take 
+                i.e chunk_size = 2 [0,1,2,3] -> [0,2]
+    overlap: is a float 0.5=50% of the overlap to take between chunk_sizes
+            chunk_size:4, np.arange(10), overlap:0.5 [0, 4, 6] -> [0, 2, 4, 6]
+    '''
+    
+    if(overlap>1 or overlap<0): raise ValueError('The Overlap Parameter must be between 0-1!')
+    
+    points = [0]
+    stride = int(chunk_size * (1-overlap))
+    counter = 1
+    while True:
+        
+        pt = stride * counter
+        
+        if pt + chunk_size >= size:
+            points.append(size - chunk_size)
+            break
+        else:
+            points.append(pt)
+        
+        counter += 1
+        
+    return points
+
+
+
+def save_numpy_out(filename, array):
+    '''
+    saves a numpy array out for later 
+    filename: 'file.npy'
+    '''
+    with open(filename, 'wb') as f:
+        np.save(f, array)
+
+
+
+def create_csv_with_tiles_and_split_points(image_raster, split_width, 
+                                           split_height, overlap,
+                                           tile_id, file_output_path, 
+                                           segmented_tiles_dir=None):
+    '''
+    Create a csv file that has the overlapping split points for each tile 
+    X0002_X0002, X0002_X0003, X0003_X0002, X0003_X0003
+    these split points are only for the sub images that have building heights
+    in them.
+    '''
+    
+    # check if the image is binary
+    is_binary = np.array_equal(image_raster, image_raster.astype(bool))  
+    # create directory if it doesn't exist
+    create_directory_to_save_to(segmented_tiles_dir)
+    
+    img_w, img_h = image_raster.shape
+    
+    X_start_points = start_points(size=img_w,
+                                  chunk_size=split_width,
+                                  overlap=overlap)
+    
+    Y_start_points = start_points(size=img_h,
+                                  chunk_size=split_height,
+                                  overlap=overlap)
+    
+    rows = []
+    for i in Y_start_points:
+        for j in X_start_points:
+            
+            X1 = i
+            X2 = (i+split_height)
+            Y1 = j
+            Y2 = (j+split_width)
+            split_img = image_raster[X1:X2, Y1:Y2]
+            
+            if not is_binary:
+                # check if not binary to calucate the area covered by buildings
+                split_binary = np.where(split_img >=1, 1, 0)
+                ratio_no_height = np.sum(split_binary)/(split_width*split_height)
+                
+            else:
+                ratio_no_height = np.sum(split_img)/(split_width*split_height)
+            
+            
+            if ratio_no_height > 0.05:
+                
+                rows.append([tile_id, X1, X2, Y1, Y2])
+                
+                if segmented_tiles_dir is not None:
+                    # if a directory is passed then save the data out.
+                    new_filename = f'Y_{tile_id}_{X1}_{X2}_{Y1}_{Y2}.npy'
+                    new_filepath = os.path.join(segmented_tiles_dir, new_filename)
+                    # expand dimension to include channel info from (250,250) -> (250,250,1)
+                    split_img = np.expand_dims(split_img, axis=2)
+                    save_numpy_out(new_filepath, split_img)
+                    
+       
+    
+    new_data = pd.DataFrame(rows, columns=['Tile_id','X1','X2','Y1','Y2'])
+    
+    if check_if_csv_file_exists(file_output_path):
+        old_data = pd.read_csv(file_output_path)
+        new_data = pd.concat([new_data, old_data], ignore_index=True, sort=False)
+        new_data.to_csv(file_output_path ,index=False)
+    
+    else:
+        print(f'No file found creating a new one at {file_output_path}')
+        new_data.to_csv(file_output_path ,index=False)
+        
+        
+    print(f'Successfuly Segmented {tile_id}')
+
+
+def check_if_csv_file_exists(path):
+        '''
+        example if path='mydirectory/myfile.csv' exists
+        return true.
+        '''
+        return os.path.exists(path)
+
+
+def extract_morphology_operater(filepath):
+    '''
+    exctracts the morphology operator from the file name
+    'C:/home/ubuntu/TEXTURE_HL_TXT_BHT.tif' -> _BHT
+    need to remove this when comapring features to band description
+    '''
+    file_name = os.path.basename(filepath)
+    clean_name = file_name.replace('.tif','')
+    
+    return '_'+clean_name.split('_')[-1]
+
+
+
+def stack_images_into_volume(list_of_img_rasters):
+    '''
+    stacks numpy arrays together along z-axis to create
+    multichannel image.
+    '''
+    return np.dstack(list_of_img_rasters)
+
+
+
+def extract_bands(directory, img_bands, important_features):
+    '''
+    given a list of band names extract the relevant tif files from the directory 
+    and stack them into a mutli channel image.
+    '''
+    
+
+    for tif in glob.glob(directory + "/*.tif"):
+        
+        filepath = tif.replace('\\','/')
+        morph = extract_morphology_operater(filepath)
+    
+        gdal_file = gdal.Open(filepath)
+                
+        for band in range(gdal_file.RasterCount):
+            band += 1
+            gdal_band = gdal_file.GetRasterBand(band)
+            name = gdal_band.GetDescription() + morph
+
+            if any(feat in name for feat in important_features):
+                raster_band = gdal_band.ReadAsArray()
+                img_bands.append(raster_band)
+                
+    return img_bands
+
+
+
+def extract_bands_and_merge(sentinel_1_asc_dir, sentinel_2_dir, important_features):
+    
+    img_band_sent_2 = []
+    img_band_sent_1 = []
+    
+    img_band_sent_2 = extract_bands(sentinel_2_dir, 
+                                    img_band_sent_2, 
+                                    important_features)
+    
+    img_band_sent_1 = extract_bands(sentinel_1_asc_dir, 
+                                    img_band_sent_1,
+                                    important_features)
+    
+    
+    img = img_band_sent_1 + img_band_sent_2
+    
+    return stack_images_into_volume(img)
+
+
+def split_image_from_csv_coords(csv_filepath, img, tile_id, output_dir):
+    '''
+    using the csv of tile coordinates created from create_csv_with_tiles_and_split_points()
+    split the X features or images sentinel-1 and 2 so that they align with the target variable
+    or building heights.s
+    '''
+    
+    if not check_if_csv_file_exists(csv_filepath): raise ValueError('The file does not exist !')
+    
+    # create directory if it doesn't exist
+    create_directory_to_save_to(output_dir)
+    
+    # filter to the appropriate tile
+    coords = pd.read_csv(csv_filepath)
+    coords = coords[coords['Tile_id'] == tile_id]
+    
+    for index, row in coords.iterrows():
+
+        X1 = row['X1']
+        X2 = row['X2']
+        Y1 = row['Y1']
+        Y2 = row['Y2']
+        tile_id = row['Tile_id']
+        
+        if img.ndim > 2:
+            split_img = img[X1:X2, Y1:Y2, :]
+            
+        else:
+            split_img = img[X1:X2, Y1:Y2]
+            
+            
+        filename = f'X_{tile_id}_{X1}_{X2}_{Y1}_{Y2}.npy'
+        filepath = os.path.join(output_dir, filename)
+        save_numpy_out(filepath, split_img)
+        
+        
+    print(f'Succesfully subseted X feature tile {tile_id} variables using csv coords, saved output to {output_dir}')
+
+        
+
 
 if __name__ == "__main__":
 
@@ -181,10 +425,49 @@ if __name__ == "__main__":
     sentinel_1_directory_asc = 'C:/Users/egnke/PythonCode/MetEireann/Sentinel-1-Data/Sentinel-1/Texture/Asc/'
     sentinel_2_directory = 'C:/Users/egnke/PythonCode/MetEireann/Sentienl-2-Data/Processed_Data/morphology/'
     settlement_map_dir = 'C:/Users/egnke/PythonCode/MetEireann/Settlement_Map/tiled/'
-    percentage = 0.1
+    percentage = 0.25
+
+ 
+    important_features = ['2020-2020_001-365_HL_TSA_SEN2L_NDV_STM_B0007_GRD', '2020-2020_001-365_HL_TSA_SEN2L_NDV_STM_B0007_ERO', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0011_OPN', '2020-2020_001-365_HL_TSA_SEN2L_BNR_STM_B0002_CLS', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0003_DIL', '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0003_GRD', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0011_CLS', '2020-2020_001-365_HL_TSA_SEN2L_TCG_STM_B0008_OPN', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_BNR_STM_B0004_OPN', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0001_DIL', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0012_DIL', '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0006_GRD', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_SW1_STM_B0002_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0007_DIL', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0002_GRD', '2020-2020_001-365_HL_TSA_SEN2L_NDW_STM_B0001_DIL', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_BNR_STM_B0005_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0005_GRD', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_GRN_STM_B0004_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0002_DIL', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0012_DIL', '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0001_DIL', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_SW1_STM_B0012_OPN', '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0003_ERO', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0003_ERO', '2020-2020_001-365_HL_TSA_SEN2L_TCW_STM_B0012_ERO', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0005_DIL', '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0007_ERO', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_BNR_STM_B0004_ERO', '2020-2020_001-365_HL_TSA_SEN2L_TCG_STM_B0006_ERO', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0005_OPN', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0007_DIL', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NDB_STM_B0007_DIL', '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0004_ERO', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NDW_STM_B0013_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0001_CLS', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_GRN_STM_B0004_GRD', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0001_OPN', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NDW_STM_B0004_DIL', '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0012_OPN', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_SW2_STM_B0002_ERO', '2020-2020_001-365_HL_TSA_SEN2L_TCB_STM_B0009_DIL', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0002_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0006_CLS', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NDW_STM_B0013_OPN', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0003_DIL', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_TCW_STM_B0007_GRD', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0011_CLS', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0012_CLS', '2020-2020_001-365_HL_TSA_SEN2L_RE2_STM_B0011_GRD', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0004_GRD', '2020-2020_001-365_HL_TSA_SEN2L_TCB_STM_B0012_CLS', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0008_GRD', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0012_GRD', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_RE1_STM_B0001_CLS', '2020-2020_001-365_HL_TSA_SEN2L_TCG_STM_B0003_OPN', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_TCG_STM_B0003_BHT', '2020-2020_001-365_HL_TSA_SEN2L_SW1_STM_B0003_ERO', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0002_CLS', '2020-2020_001-365_HL_TSA_SEN2L_NDB_STM_B0011_CLS', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0009_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0007_CLS', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_SW1_STM_B0007_ERO', '2020-2020_001-365_HL_TSA_SEN2L_TCB_STM_B0013_ERO', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_SW1_STM_B0004_ERO', '2020-2020_001-365_HL_TSA_SEN2L_RE1_STM_B0011_ERO', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0001_GRD', '2020-2020_001-365_HL_TSA_SEN2L_RE1_STM_B0011_OPN', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0001_CLS', '2020-2020_001-365_HL_TSA_SEN2L_TCB_STM_B0004_ERO', 
+                     '2020-2020_001-365_HL_TSA_SEN2L_NIR_STM_B0001_ERO', '2020-2020_001-365_HL_TSA_VVVHP_BVV_STM_B0007_OPN', 
+                     '2020-2020_001-365_HL_TSA_VVVHP_BVH_STM_B0007_GRD', '2020-2020_001-365_HL_TSA_SEN2L_RE2_STM_B0008_ERO']
 
 
-    sub_dirs = ['X0002_Y0002','X0002_Y0003','X0003_Y0002','X0003_Y0003' ]
+    sub_dirs = ['X0002_Y0002','X0002_Y0003','X0003_Y0002']
 
 
     list_of_dfs = []
@@ -222,6 +505,39 @@ if __name__ == "__main__":
     
     startified_sample = pd.concat(list_of_dfs)
     startified_sample.to_csv('building_height_sample_asc.csv', index=False)
+    
+    ##########################################################################################################
+    ##########################################################################################################
+
+    # getting the test dataset in the same format as unet.
+    test_dir = 'X0003_Y0003'
+    csv_output_path = 'C:/Users/egnke/PythonCode/Met_Eireann_git/Ireland_building_heights/Modelling/DataSetCreation/Pixel-Wise-Data/pixel_building_height_testing_coords.csv'
+    segmented_tiles_dir_Y = 'C:/Users/egnke/PythonCode/Met_Eireann_git/Ireland_building_heights/Modelling/DataSetCreation/Pixel-Wise-Data/y_test/'
+    segmented_tiles_dir_X = 'C:/Users/egnke/PythonCode/Met_Eireann_git/Ireland_building_heights/Modelling/DataSetCreation/Pixel-Wise-Data/X_test/'
+
+    building_height_dir = building_height_directory + test_dir
+    sentinel_1_asc_dir  = sentinel_1_directory_asc + test_dir
+    sentinel_2_dir  = sentinel_2_directory + test_dir
+    settle_map_dir  = settlement_map_dir + test_dir
+
+    building_height = glob.glob(building_height_dir+"/*.tif")[0].replace('\\','/')
+    build_height = gdal.Open(building_height)
+    height_data = build_height.GetRasterBand(1).ReadAsArray()
+
+    create_csv_with_tiles_and_split_points(height_data, 250, 250, 0,
+                                           test_dir, csv_output_path, 
+                                           segmented_tiles_dir=segmented_tiles_dir_Y)
+
+    img = extract_bands_and_merge(sentinel_1_asc_dir, sentinel_2_dir, important_features)
+
+    split_image_from_csv_coords(csv_output_path, img, test_dir, segmented_tiles_dir_X)
+
+
+    
+    
+
+
+
 
 
 
